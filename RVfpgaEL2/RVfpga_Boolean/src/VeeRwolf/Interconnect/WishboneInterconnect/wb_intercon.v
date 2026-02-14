b032369baed139a8643791c497e57f13823a1c3e
Reece Wayt <reecewayt96@gmail.com>
2026-02-13 16:38:07 -0800

    feat(RTL) - Generated new wb interconnect signals for vga peripheral

--- RVfpgaEL2/RVfpga_Boolean/src/VeeRwolf/Interconnect/WishboneInterconnect/wb_intercon.v
+++ RVfpgaEL2/RVfpga_Boolean/src/VeeRwolf/Interconnect/WishboneInterconnect/wb_intercon.v
@@ -63,6 +63,18 @@ module wb_intercon
     input         wb_gpio_ack_i,
     input         wb_gpio_err_i,
     input         wb_gpio_rty_i,
+    output [31:0] wb_vga_adr_o,
+    output [31:0] wb_vga_dat_o,
+    output  [3:0] wb_vga_sel_o,
+    output        wb_vga_we_o,
+    output        wb_vga_cyc_o,
+    output        wb_vga_stb_o,
+    output  [2:0] wb_vga_cti_o,
+    output  [1:0] wb_vga_bte_o,
+    input  [31:0] wb_vga_dat_i,
+    input         wb_vga_ack_i,
+    input         wb_vga_err_i,
+    input         wb_vga_rty_i,
     output [31:0] wb_gpio2_adr_o,
     output [31:0] wb_gpio2_dat_o,
     output  [3:0] wb_gpio2_sel_o,
@@ -101,9 +113,9 @@ module wb_intercon
     input         wb_uart_rty_i);
 
 wb_mux
-  #(.num_slaves (7),
-    .MATCH_ADDR ({32'h00000000, 32'h00001000, 32'h00001200, 32'h00001400, 32'h00001800, 32'h00001c00, 32'h00002000}),
-    .MATCH_MASK ({32'hfffff000, 32'hffffffc0, 32'hffffffc0, 32'hffffffc0, 32'hffffffc0, 32'hffffffc0, 32'hfffff000}))
+  #(.num_slaves (8),
+    .MATCH_ADDR ({32'h00000000, 32'h00001000, 32'h00001200, 32'h00001400, 32'h00001500, 32'h00001800, 32'h00001c00, 32'h00002000}),
+    .MATCH_MASK ({32'hfffff000, 32'hffffffc0, 32'hffffffc0, 32'hffffffc0, 32'hffffffc0, 32'hffffffc0, 32'hffffffc0, 32'hfffff000}))
  wb_mux_io
    (.wb_clk_i  (wb_clk_i),
     .wb_rst_i  (wb_rst_i),
@@ -119,17 +131,17 @@ wb_mux
     .wbm_ack_o (wb_io_ack_o),
     .wbm_err_o (wb_io_err_o),
     .wbm_rty_o (wb_io_rty_o),
-    .wbs_adr_o ({wb_rom_adr_o, wb_sys_adr_o, wb_ptc_adr_o, wb_gpio_adr_o, wb_gpio2_adr_o, wb_rgb_adr_o, wb_uart_adr_o}),
-    .wbs_dat_o ({wb_rom_dat_o, wb_sys_dat_o, wb_ptc_dat_o, wb_gpio_dat_o, wb_gpio2_dat_o, wb_rgb_dat_o, wb_uart_dat_o}),
-    .wbs_sel_o ({wb_rom_sel_o, wb_sys_sel_o, wb_ptc_sel_o, wb_gpio_sel_o, wb_gpio2_sel_o, wb_rgb_sel_o, wb_uart_sel_o}),
-    .wbs_we_o  ({wb_rom_we_o, wb_sys_we_o, wb_ptc_we_o, wb_gpio_we_o, wb_gpio2_we_o, wb_rgb_we_o, wb_uart_we_o}),
-    .wbs_cyc_o ({wb_rom_cyc_o, wb_sys_cyc_o, wb_ptc_cyc_o, wb_gpio_cyc_o, wb_gpio2_cyc_o, wb_rgb_cyc_o, wb_uart_cyc_o}),
-    .wbs_stb_o ({wb_rom_stb_o, wb_sys_stb_o, wb_ptc_stb_o, wb_gpio_stb_o, wb_gpio2_stb_o, wb_rgb_stb_o, wb_uart_stb_o}),
-    .wbs_cti_o ({wb_rom_cti_o, wb_sys_cti_o, wb_ptc_cti_o, wb_gpio_cti_o, wb_gpio2_cti_o, wb_rgb_cti_o, wb_uart_cti_o}),
-    .wbs_bte_o ({wb_rom_bte_o, wb_sys_bte_o, wb_ptc_bte_o, wb_gpio_bte_o, wb_gpio2_bte_o, wb_rgb_bte_o, wb_uart_bte_o}),
-    .wbs_dat_i ({wb_rom_dat_i, wb_sys_dat_i, wb_ptc_dat_i, wb_gpio_dat_i, wb_gpio2_dat_i, wb_rgb_dat_i, wb_uart_dat_i}),
-    .wbs_ack_i ({wb_rom_ack_i, wb_sys_ack_i, wb_ptc_ack_i, wb_gpio_ack_i, wb_gpio2_ack_i, wb_rgb_ack_i, wb_uart_ack_i}),
-    .wbs_err_i ({wb_rom_err_i, wb_sys_err_i, wb_ptc_err_i, wb_gpio_err_i, wb_gpio2_err_i, wb_rgb_err_i, wb_uart_err_i}),
-    .wbs_rty_i ({wb_rom_rty_i, wb_sys_rty_i, wb_ptc_rty_i, wb_gpio_rty_i, wb_gpio2_rty_i, wb_rgb_rty_i, wb_uart_rty_i}));
+    .wbs_adr_o ({wb_rom_adr_o, wb_sys_adr_o, wb_ptc_adr_o, wb_gpio_adr_o, wb_vga_adr_o, wb_gpio2_adr_o, wb_rgb_adr_o, wb_uart_adr_o}),
+    .wbs_dat_o ({wb_rom_dat_o, wb_sys_dat_o, wb_ptc_dat_o, wb_gpio_dat_o, wb_vga_dat_o, wb_gpio2_dat_o, wb_rgb_dat_o, wb_uart_dat_o}),
+    .wbs_sel_o ({wb_rom_sel_o, wb_sys_sel_o, wb_ptc_sel_o, wb_gpio_sel_o, wb_vga_sel_o, wb_gpio2_sel_o, wb_rgb_sel_o, wb_uart_sel_o}),
+    .wbs_we_o  ({wb_rom_we_o, wb_sys_we_o, wb_ptc_we_o, wb_gpio_we_o, wb_vga_we_o, wb_gpio2_we_o, wb_rgb_we_o, wb_uart_we_o}),
+    .wbs_cyc_o ({wb_rom_cyc_o, wb_sys_cyc_o, wb_ptc_cyc_o, wb_gpio_cyc_o, wb_vga_cyc_o, wb_gpio2_cyc_o, wb_rgb_cyc_o, wb_uart_cyc_o}),
+    .wbs_stb_o ({wb_rom_stb_o, wb_sys_stb_o, wb_ptc_stb_o, wb_gpio_stb_o, wb_vga_stb_o, wb_gpio2_stb_o, wb_rgb_stb_o, wb_uart_stb_o}),
+    .wbs_cti_o ({wb_rom_cti_o, wb_sys_cti_o, wb_ptc_cti_o, wb_gpio_cti_o, wb_vga_cti_o, wb_gpio2_cti_o, wb_rgb_cti_o, wb_uart_cti_o}),
+    .wbs_bte_o ({wb_rom_bte_o, wb_sys_bte_o, wb_ptc_bte_o, wb_gpio_bte_o, wb_vga_bte_o, wb_gpio2_bte_o, wb_rgb_bte_o, wb_uart_bte_o}),
+    .wbs_dat_i ({wb_rom_dat_i, wb_sys_dat_i, wb_ptc_dat_i, wb_gpio_dat_i, wb_vga_dat_i, wb_gpio2_dat_i, wb_rgb_dat_i, wb_uart_dat_i}),
+    .wbs_ack_i ({wb_rom_ack_i, wb_sys_ack_i, wb_ptc_ack_i, wb_gpio_ack_i, wb_vga_ack_i, wb_gpio2_ack_i, wb_rgb_ack_i, wb_uart_ack_i}),
+    .wbs_err_i ({wb_rom_err_i, wb_sys_err_i, wb_ptc_err_i, wb_gpio_err_i, wb_vga_err_i, wb_gpio2_err_i, wb_rgb_err_i, wb_uart_err_i}),
+    .wbs_rty_i ({wb_rom_rty_i, wb_sys_rty_i, wb_ptc_rty_i, wb_gpio_rty_i, wb_vga_rty_i, wb_gpio2_rty_i, wb_rgb_rty_i, wb_uart_rty_i}));
 
 endmodule
